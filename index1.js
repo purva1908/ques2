577fdd9cfea1cc502b98063c1c6af674fa2d4c02
Purva Tayde <72730647+purva1908@users.noreply.github.com>
2021-07-07 00:56:40 +0530

    added completed and working mini app

---
+++ index1.js
@@ -0,0 +1,26 @@
+import express from "express"
+import fs from "fs"
+import bodyParser from "body-parser"
+
+
+const server = express()
+server.use(bodyParser.json())
+
+server.use("/", myFunc)
+server.listen(8080)
+
+function myFunc(req,res){
+     console.log("connected")
+     fs.readFile("user1.json", function(err, data) {
+      
+        // Check for errors
+        if (err) throw err;
+       
+        // Converting to JSON
+        const users = JSON.parse(data);
+          
+        console.log(users); // Print users 
+        res.send(users)
+    });
+}
+
